6b962cef2a9f6379075875ee076670be44a86eb1
Steven von Dohlen <vondoste@live.unc.edu>
2021-02-05 14:10:29 -0500

    passes autograder, rounding instead of remainder.

--- exercises/ex01/vaccine_calc.py
+++ exercises/ex01/vaccine_calc.py
@@ -22,24 +22,28 @@ from datetime import timedelta
 population: int = 2 * int(input("Population: "))  # keep number of doses needed (2 doses per person).
 doses_admin: int = int(input("Doses Administered: "))    # keep doses administered here
 doses_per_day: int = int(input("Doses per day: "))   # keep doses per day here
-target: int = int(input("Target percent vaccinated: "))  # keep target percentage here
+target_percent: int = int(input("Target percent vaccinated: "))  # keep target percentage here
+target_doses: int = round(population * (target_percent / 100))  # How many doses total to reach target percentage.
 today: datetime = datetime.today()  # keep today date here
 one_day: timedelta = timedelta(1)   # store a one day interval here
 completion_date: datetime = datetime.today()    # store completion date here, initialize it to today.
-current_percentage: float = (doses_admin / population) * 100  # calculate current percentage vaccinated, store here
-percent_left: float = target - current_percentage  # calc & store % left to go, neg number if already exceeded target.
-percent_per_day: float = (doses_per_day / population) * 100  # calculate percent per day and store here
+doses_left: int = target_doses - doses_admin  # How many doses left to give.
 days_left: int = 0  # holding place for number of days left
 
+print("target_doses = " + str(target_doses))  # What's in the variable?
+print("doses_left = " + str(doses_left))  # What's in the variable?
 
 # Have to decide if we have already reached the target percentage or not.
-if percent_left > 0:    # Have not reached the target yet, time to figure out when we will
-    days_left = int(percent_left // percent_per_day)  # Calculate the number of whole days left to reach target
-    if 0 < percent_left % percent_per_day:  # is there a fraction of a day left after the division to account for?
-        days_left = days_left + 1   # add one to the day counter for the fractional day needed.
+# left my original calculation in comments.  Adding a day for the remainder is more precise than rounding.
+if doses_left > 0:    # Have not reached the target yet, time to figure out when we will
+    # days_left = doses_left // doses_per_day  # Calculate the number of whole days left to reach target
+    # if 0 < doses_left % doses_per_day:  # is there a fraction of a day left after the division to account for?
+    #     print("remainder from days_left = " + str(doses_left % doses_per_day))  # See what the remainder is.
+    #     days_left = days_left + 1   # add one to the day counter for the fractional day needed.
+    days_left = round(doses_left / doses_per_day)  # Alternate calculation, rounding is less precise.
 
     completion_date = today + (one_day * days_left)  # add the number of days left to today's date.
 
-
-print("We will reach " + str(target) + "% vaccination in " + str(days_left) + 
-     " days, which falls on " + completion_date.strftime("%B %d, %Y") + ".")
+result1: str = "We will reach " + str(target_percent) + "% vaccination in " + str(days_left) + " days, "  # part 1
+result2: str = "which falls on " + completion_date.strftime("%B %d, %Y") + "."  # part 2
+print(result1 + result2)  # Had to break result string composition into two parts to fit in 119 characters
