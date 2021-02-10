bf2988571df475e9e16559022729176fddba9dc1
Steven von Dohlen <vondoste@live.unc.edu>
2021-02-09 19:45:27 -0500

    Fortune cookie program from EX01 made into a fn

--- exercises/ex02_p1/fortune_cookie.py
+++ exercises/ex02_p1/fortune_cookie.py
@@ -8,11 +8,35 @@ __author__ = "730366999"
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     print("Your fortune cookie says...")
-    # TODO 2: Print the result of calling your fortune_cookie function.
+    print(fortune_cookie())  # Should print the string returned by fortune_cookie.
     print("Now, go spread positive vibes!")
 
 
-# TODO 1: Define your fortune_cookie function here.
+def fortune_cookie() -> str:  # Define fortune_cookie function.
+    fortune: str = "What do you want for nothing, rubber biscuit?"  # Initialize variable to hold fortune.
+    msg: int = randint(1, 5)  # Declare msg as integer, and initialize it with random number from 1 to 5.
+
+    # The following set of if statements will set fortune to be a message depending on what value msg has.
+    if msg < 2:  # detects msg == 1
+        fortune = "Knowing something, and thinking you know both feel the same until you are proven wrong."
+
+    else:
+        if msg < 3:  # detects msg == 2
+            fortune = "'Should be' and 'Is be' ain't necessarily the same thing."
+
+        else:
+            if msg < 4:  # detects msg == 3
+                fortune = "If you don't know why you are doing something, maybe you shouldn't be doing it."
+
+            else:
+                if msg < 5:  # detects msg == 4
+                    fortune = "No matter where you go, there you are."
+
+                else:   # detects if msg == 5
+                    fortune = "May the bluebird of happiness leave glad tidings upon your shoulder."
+    
+    return fortune
+
 
 
 # Python Idiom for "starting" the program when run as a module.
