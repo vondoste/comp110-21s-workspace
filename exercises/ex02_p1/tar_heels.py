7f2df15a85a8ac8497d93ab03a65880b0792d7d0
Steven von Dohlen <vondoste@live.unc.edu>
2021-02-09 20:09:06 -0500

    Tar_heels converted to a function for EX02

--- exercises/ex02_p1/tar_heels.py
+++ exercises/ex02_p1/tar_heels.py
@@ -6,10 +6,27 @@ __author__ = "730366999"
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     choice: int = int(input("Enter an int: "))
-    # TODO 2: Print the response of calling the tar_heels function here.
+    print(tar_heels(choice))
 
 
-# TODO 1: Define the tar_heels function, and its logic, here.
+def tar_heels(number: int) -> str:  # Define tar_heels function.  Takes one int, returns a str.
+    result: str = "insert result here."  # Declare variable to hold my result
+
+    if 0 == number % 2:  # is number evenly divisible by 2?
+        if 0 == number % 7:  # yes, is number also evenly divisible by 7?
+            result = "TAR HEELS"
+
+        else:   # no, not also evenly divisible by 7
+            result = "TAR"
+
+    else:   # not evenly divisible by 2
+        if 0 == number % 7:  # is number evenly divisible by 7?
+            result = "HEELS"
+
+        else:   # not evenly divisible by either 2 or 7
+            result = "CAROLINA"
+
+    return result
 
 
 if __name__ == "__main__":
