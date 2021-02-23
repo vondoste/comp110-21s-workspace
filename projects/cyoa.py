a19ab065e5b6e293ef0c636942821886ea6794db
Steven von Dohlen <vondoste@live.unc.edu>
2021-02-22 20:30:16 -0500

    Added more banter, and the actual gameplay

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -15,10 +15,10 @@ def main() -> None:
     """main function - Our entry point to the virtual world"""
     global points
     
-    points = 0  # Everybody starts with nothing. - verify that this is right!
+    points = 0  # Everybody starts with nothing. 
     # Insert a loop somewhere in main()
     greet()
-    decision: int = int(input(f"Hey { player }, whatcha want to do? (1=bet/2=play/3=sell/4 or more = anything else."))  # supply details later
+    decision: int = int(input(f"Hey { player }, whatcha want to do? (1=bet/2=play/3=sell/4 or more = take a hike."))  # supply details later
     if decision == 1:
         
         # procedure call (put your pocket money on the table, or sell an item to get money
@@ -41,18 +41,39 @@ def main() -> None:
 
 
 def greet() -> None:
-    """docstring"""
-    print("Step right up, try your luck, every game has a winner!")  # Decide on snazzy greeting
+    """Say hello, get the players name."""
     global player
-    
+
+    print("Step right up, try your luck, every game has a winner!")  # Decide on snazzy greeting    
     player = str(input("Hey Sport, what's your name?")) # Lookup the details of how to do this
+
+
 
 
 def play_game(points: int) -> int:
     """ball finding begins here"""
     from random import randint
+    choice: int = 0
+    ball: int = randint(1,3)
     # do something to points
-    # 
+    print("I have hidden the ball under one of these three cups.")
+    print("  \U0001F95B \U0001F95B \U0001F95B ")
+    choice = int(input("Double or nothing, which cup is the ball under (1,2, or 3)?"))
+    if ball < 3:  # Display which of the 3 cups has the ball
+        if ball < 2:
+            print("  \U0001F7E2 \U0001F95B \U0001F95B  ")
+        else:
+            print("  \U0001F95B \U0001F7E2 \U0001F95B  ")
+    else:
+        print("  \U0001F95B \U0001F95B \U0001F7E2 ")
+
+    if choice == ball:
+        print("We have a Winner! Way to double your money.")
+        points = points * 2
+    else:
+        print("Hey, better luck next time.")
+        points = 0
+
     return points
 
 
