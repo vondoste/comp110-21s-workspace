80f50c74f0f8ea9e9ba0482ba048051b664bc573
Steven von Dohlen <vondoste@live.unc.edu>
2021-04-20 17:36:16 -0400

    LS25 class lecture files

---
+++ lessons/ls25_diagram0_part0.py
@@ -0,0 +1,32 @@
+"""A class for rational numbers."""
+from __future__ import annotations
+
+class Rat:
+    n: int
+    d: int
+
+    def __init__(self, n: int, d: int) -> None:
+        self.n = n
+        self.d = d
+
+    def __repr__(self) -> str:
+        return f"{self.n}/{self.d}"
+    
+    def simplify(self) -> Rat:
+        gcd = self.d // 2
+        while gcd > 1:
+            if self.n % gcd == 0 and self.d % gcd == 0:
+                return Rat(self.n // gcd, self.d // gcd)
+            else:
+                gcd -= 1
+        return self
+
+
+x = Rat(6, 8)
+y = Rat(1, 2)
+
+simp_y = y.simplify()
+print(simp_y)
+
+simp_x = x.simplify()
+print(simp_x)
---
+++ lessons/ls25_diagram0_part1.py
@@ -0,0 +1,24 @@
+"""A class for rational numbers."""
+from __future__ import annotations
+
+class Rat:
+    n: int
+    d: int
+
+    def __init__(self, n: int, d: int) -> None:
+        self.n = n
+        self.d = d
+
+    def __repr__(self) -> str:
+        return f"{self.n}/{self.d}"
+
+    def __add__(self, rhs: Rat) -> Rat:
+        n: int = self.n * rhs.d + rhs.n * self.d
+        d: int = self.d * rhs.d
+        return Rat(n, d)
+
+
+x = Rat(1, 5)
+y = Rat(0, 4)
+z = y + x
+print(z)
