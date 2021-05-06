69e0b54b522eaa4a2c4e50c9a9990f9ff1aa4084
Steven von Dohlen <vondoste@live.unc.edu>
2021-05-06 08:03:18 -0400

    playing with parameters

--- lessons/ls28_art.py
+++ lessons/ls28_art.py
@@ -22,15 +22,16 @@ def branch(length: float, angle: float) -> None:
         ... # Base case: do nothing
     else:
         # Recursive case: Branch again
-        branch(length * 0.6, angle + 25)
-        branch(length * 0.5, angle - 20)
+        # branch(length * 0.3, angle)
+        branch(length * 0.56, angle + 35)
+        branch(length * 0.9, angle - 30)
     # Bring the turtle back to it's starting point
     t.setheading(angle + 180.0)
     t.forward(length)
 
 
 if __name__ == "__main__":
-    t.speed(0)
+    t.speed(3)
     t.getscreen().tracer(0, 0)
     t.getscreen().onclick(tree)
     done()
